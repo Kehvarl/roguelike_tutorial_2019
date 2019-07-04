ee55a72dfdb415db356e487d1be739ac2bb22525
Kehvarl <kehvarl@kehvarl.com>
2019-07-04 13:12:56 -0700

    Create Entity object

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import tcod as libtcod
+from entity import Entity
 from input_handlers import handle_keys
 
 
@@ -6,8 +7,9 @@ def main():
     screen_width = 80
     screen_height = 50
 
-    player_x = 25
-    player_y = 40
+    player = Entity(35, 25, '@', libtcod.white)
+    npc = Entity(30,30, 'N', libtcod.yellow)
+    entities = [npc, player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
@@ -21,11 +23,11 @@ def main():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
         libtcod.console_set_default_foreground(con, libtcod.white)
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, player.x, player.y, player.char, libtcod.BKGND_NONE)
         libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
         libtcod.console_flush()
 
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, player.x, player.y, ' ', libtcod.BKGND_NONE)
 
         action = handle_keys(key)
 
@@ -35,9 +37,7 @@ def main():
 
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
-
+            player.move(dx, dy)
         if exit:
             return True
 
