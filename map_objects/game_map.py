146e48c735559d7cb332d733a02f73268ffb24a6
Kehvarl <kehvarl@kehvarl.com>
2019-07-07 10:23:43 -0700

    Game Map object

---
+++ map_objects/game_map.py
@@ -0,0 +1,21 @@
+from map_objects.tile import Tile
+
+
+class GameMap:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.tiles = self.initialize_tiles()
+
+    def initialize_tiles(self):
+        tiles = [[Tile(False) for _ in range(self.height)] for _ in range(self.width)]
+
+        tiles[30][22].blocked = True
+        tiles[30][22].block_sight = True
+        tiles[31][22].blocked = True
+        tiles[31][22].block_sight = True
+        tiles[32][22].blocked = True
+        tiles[32][22].block_sight = True
+
+        return tiles
+
