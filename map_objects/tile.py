622920821a3e721b08ea34d75f2f6056983ecb62
Kehvarl <kehvarl@kehvarl.com>
2019-07-07 10:15:58 -0700

    Create Tile primitive

---
+++ map_objects/tile.py
@@ -0,0 +1,9 @@
+class Tile:
+    def __init__(self, blocked=False, block_sight=None):
+        self.blocked = blocked
+        
+        if block_sight is None:
+            block_sight = blocked
+
+        self.block_sight = block_sight
+
