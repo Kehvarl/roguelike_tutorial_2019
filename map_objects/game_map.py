63f93c5c47752f690973d7e9a6cbbcea85937fc9
Kehvarl <kehvarl@kehvarl.com>
2019-07-07 10:35:34 -0700

    update map_init to prepare for random generation

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -8,14 +8,7 @@ class GameMap:
         self.tiles = self.initialize_tiles()
 
     def initialize_tiles(self):
-        tiles = [[Tile(False) for _ in range(self.height)] for _ in range(self.width)]
-
-        tiles[30][22].blocked = True
-        tiles[30][22].block_sight = True
-        tiles[31][22].blocked = True
-        tiles[31][22].block_sight = True
-        tiles[32][22].blocked = True
-        tiles[32][22].block_sight = True
+        tiles = [[Tile(True) for _ in range(self.height)] for _ in range(self.width)]
 
         return tiles
 
