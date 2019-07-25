45e281df31543c0621b6f3f1ec4faf686d889585
Kehvarl <kehvarl@kehvarl.com>
2019-07-24 19:57:30 -0700

    display entities under mouse cursor

--- engine.py
+++ engine.py
@@ -66,12 +66,12 @@ def main():
     game_state = GameStates.PLAYER_TURN
 
     while not libtcod.console_is_window_closed():
-        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
         
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 
-        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height, bar_width, panel_height, panel_y, colors)
+        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height, bar_width, panel_height, panel_y, mouse, colors)
 
         libtcod.console_flush()
 
