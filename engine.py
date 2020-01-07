3d8a48e6981b15d43a253319dff24351bc626609
Adam Corbett <kehvarl@gmail.com>
2020-01-06 20:32:34 -0700

    pep8 cleanup

--- engine.py
+++ engine.py
@@ -4,6 +4,7 @@ from input_handlers import handle_keys
 from map_objects.game_map import GameMap
 from render_functions import render_all, clear_all
 
+
 def main():
     screen_width = 80
     screen_height = 50
@@ -15,12 +16,12 @@ def main():
     max_rooms = 30
 
     colors = {
-        'dark_wall': libtcod.Color(0,0,100),
-        'dark_ground': libtcod.Color(50,50,150)
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
     }
 
     player = Entity(40, 25, '@', libtcod.white)
-    npc = Entity(25,20, 'N', libtcod.yellow)
+    npc = Entity(25, 20, 'N', libtcod.yellow)
     entities = [npc, player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -46,17 +47,17 @@ def main():
         action = handle_keys(key)
 
         move = action.get('move')
-        exit = action.get('exit')
-        fullscreen = action.get('fullscreen')
+        exit_game = action.get('exit')
+        full_screen = action.get('fullscreen')
 
         if move:
             dx, dy = move
             if not game_map.is_blocked(player.x + dx, player.y + dy):
                 player.move(dx, dy)
-        if exit:
+        if exit_game:
             return True
 
-        if fullscreen:
+        if full_screen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
 
